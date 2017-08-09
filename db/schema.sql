4bc6c6c02a2a95213f0e12a5d81d19cb38a88806
Harrison McCord <harrison.mccord@gmail.com>
2017-08-09 14:55:27 -0400

    schema done

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
-CREATE DATABASE burgers_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
+DROP TABLE IF EXISTS  burgers;
+
 CREATE TABLE burgers (
-id INT AUTO_INCREMENT,
+id INT AUTO_INCREMENT NOT NULL,
 burger_name VARCHAR(50) NOT NULL,
-devoured,
-date
-)
+devoured BOOL DEFAULT false,
+);
